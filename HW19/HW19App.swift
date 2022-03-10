8d193193ec4ca8597d5af29e8e5d06266c758a53
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2022-03-10 19:01:09 +0300

    Initial Commit

---
+++ HW19/HW19App.swift
@@ -0,0 +1,17 @@
+//
+//  HW19App.swift
+//  HW19
+//
+//  Created by Александр Петрович on 10.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
