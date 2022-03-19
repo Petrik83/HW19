96d9a274a2ce107ad60161106b2a74ca8f502c74
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2022-03-19 19:37:57 +0300

    Clean code.

--- HW19/Sourses/Scenes/FindView/SearchScreen/SearchView.swift
+++ HW19/Sourses/Scenes/FindView/SearchScreen/SearchView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct SearchView: View {
     
-//    var pickerChoices = ["Apple Music", "Ваша Медиатека"]
     var columns = [GridItem(.flexible())]
     @EnvironmentObject var picker: PickerChoise
     @EnvironmentObject var searchText: SearchText
@@ -17,16 +16,6 @@ struct SearchView: View {
     
     var body: some View {
         VStack {
-//            Text("khjghg")
-//            Picker(selection: $picker.pickerSelection, label: Text("")) {
-//                ForEach(pickerChoices, id: \.self) {
-//                    Text($0)
-//                }
-//            }
-//            .pickerStyle(.segmented)
-//            .padding(.horizontal, SearchViewMetric.horizontalPadding)
-//            Spacer()
-            
             if searchText.searchText == "" {
                 if !searchText.lastSearch.isEmpty {
                     LastSearchBar()
@@ -126,7 +115,6 @@ struct SearchHintsButton: View {
     var item = String()
     
     var body: some View {
-        
         Button {
             searchText.searchText = item
             searchText.hintButtonDidPressed = true
@@ -142,7 +130,6 @@ struct SearchHintsButton: View {
             }
         }
     }
-    
 }
 
 struct SearchFilterButton: View {
@@ -151,7 +138,6 @@ struct SearchFilterButton: View {
     var item: SectionItem
     
     var body: some View {
-        
         Button {
             searchText.searchResult = item
             playerPresenter.timerSlider = 0
@@ -159,15 +145,12 @@ struct SearchFilterButton: View {
             UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         } label: {
             SearchResultCell(SearchResultCellItem: item)
-            
         }
     }
-    
 }
 
 enum SearchViewMetric {
     static let horizontalPadding = 20.0
     static let trailingPadding = 10.0
     static let spacing = 0.0
-    
 }
--- HW19/Sourses/Scenes/FindView/UIKit/FindViewControllerSUI.swift
+++ HW19/Sourses/Scenes/FindView/UIKit/FindViewControllerSUI.swift
@@ -10,31 +10,32 @@ import UIKit
 
 struct FindViewControllerSUI: UIViewControllerRepresentable {
     
-//    @EnvironmentObject var playerPresenter: PlayerPresenter
     @Binding var showPlayer: Bool?
     @EnvironmentObject var searchText: SearchText
     let controller = UINavigationController(rootViewController: FindViewController())
-
-    func makeCoordinator() -> Coordinator {
-        return FindViewControllerSUI.Coordinator(parent: self)
-    }
     
     func makeUIViewController(context: Context) -> UIViewController {
         let searchView = UIHostingController(rootView: SearchView())
-        
         let searchController = UISearchController(searchResultsController: searchView)
-//        searchController.searchBar.text = "hkjhj"
-        searchController.showsSearchResultsController = true
-        searchController.searchBar.scopeButtonTitles = ["Apple Music", "Ваша Медиатека"]
-        searchController.searchBar.delegate = context.coordinator
-        
         controller.navigationBar.topItem?.hidesSearchBarWhenScrolling = false
         controller.navigationBar.topItem?.searchController = searchController
-        
+        searchController.searchBar.delegate = context.coordinator
+        setupSearchController(searchController: searchController)
         return controller
     }
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return FindViewControllerSUI.Coordinator(parent: self)
+    }
+    
+    private func setupSearchController(searchController: UISearchController) {
+        searchController.showsSearchResultsController = true
+        searchController.searchBar.scopeButtonTitles = ["Apple Music", "Ваша Медиатека"]
+        searchController.searchBar.selectedScopeButtonIndex = 0
+        searchController.searchBar.placeholder = searchController.searchBar.scopeButtonTitles?[searchController.searchBar.selectedScopeButtonIndex]
     }
 }
 
@@ -67,6 +68,10 @@ extension FindViewControllerSUI {
         func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
             parent.showPlayer = true
             parent.controller.navigationBar.topItem?.searchController?.searchBar.text = ""
+        }
+        
+        func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+            parent.controller.navigationBar.topItem?.searchController?.searchBar.placeholder = parent.controller.navigationBar.topItem?.searchController?.searchBar.scopeButtonTitles?[selectedScope]
         }
     }
 }
